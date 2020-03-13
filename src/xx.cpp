0b6a04dfc7e269d1e5d38af1d2f10e01385bda30
ToraNova <chia_jason96@live.com>
2020-03-14 03:39:24 +0800

    added cpp example

---
+++ src/xx.cpp
@@ -0,0 +1,11 @@
+/*
+ * CPP test
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]){
+	cout << "This executable is compiled from a cpp source" << endl;
+	return 0;
+}
